2364c003cb9da9f173d1d95cb339ec0d2e73fb69
Joshua Patterson <joshkpatt@gmail.com>
2022-11-09 23:23:05 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import { Button, Container, Navbar, Modal } from "react-bootstrap";
+import { useState, useContext } from "react";
+import { CartContext } from "../CartContext";
+
+function NavbarComponent() {
+  const cart = useContext(CartContext);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const productsCount = cart.items.reduce(
+    (sum, product) => sum + product.quantity,
+    0
+  );
+
+  return (
+    <>
+      <Navbar expand="sm">
+        <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Button onClick={handleShow}>Cart ({productsCount}) Items</Button>
+        </Navbar.Collapse>
+      </Navbar>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Shopping Cart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h1>body</h1>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default NavbarComponent;
